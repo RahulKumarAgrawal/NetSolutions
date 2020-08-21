fff69e12d8a8b0561fad76adf6fd2135df3a674f
Rahul Kumar Agrawal <38567875+RahulKumarAgrawal@users.noreply.github.com>
2020-08-21 17:39:53 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,10 @@
-def CountFrequency(myList): 
-      
-   
-   count = {} 
-   for i in [1,1,2,3,4,5,3,2,3,4,2,1,2,3]: 
-    count[i] = count.get(i, 0) + 1
-   return count 
-  
- 
- 
-myList =[1,1,2,3,4,5,3,2,3,4,2,1,2,3] 
+def CountFrequency(myList):
+    count = {}
+    for i in [1, 1, 2, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3]:
+        count[i] = count.get(i, 0) + 1
+    return count
+
+
+myList = [1, 1, 2, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3]
 print(CountFrequency(myList))
+
