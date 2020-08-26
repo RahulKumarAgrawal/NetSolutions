c3c19943ada8c97ba61810acb0e0a97d5479967a
Rahul Kumar Agrawal <38567875+RahulKumarAgrawal@users.noreply.github.com>
2020-08-26 13:19:23 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,9 @@
-def CountFrequency(myList):
+"""
+function to count the frequency of each elements.
+"""
+def count_frequency(mylist):
+    """Return count of each elements."""
     count = {}
     for i in [1, 1, 2, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3]:
         count[i] = count.get(i, 0) + 1
     return count
-
-
-myList = [1, 1, 2, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3]
-print(CountFrequency(myList))
-
